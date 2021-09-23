4f9e1c3ab1f9e324e1b2bf584b116dd779c438a1
alexandra-costa <alexandraaraujodacosta@gmail.com>
2021-09-23 17:27:13 -0300

    first commit

---
+++ cypress/integration/arcade.spec.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+
+describe('User', () => {
+
+    it('should be able to navigate on the home page', () => {
+        cy.visit('http://127.0.0.1:5500/index.html');
+        cy.get('[id=home]').click();
+        cy.get('[id=busca]').type('Street Fighter').clear();
+        cy.get('[id=contact]').click();
+    });
+    it('should be able to subscribe on the Arcade website', () => {
+        cy.visit('http://127.0.0.1:5500/contact.html');
+        cy.get('[id=name1]').type('Alexandra Araujo da Costa');
+        cy.get('[id=email1]').type('alexandra@startaideia.com.br');
+        cy.get('[id=tel1]').type('986213464');
+        cy.get('[id=select1]').select('PacMan');
+        cy.get('[id=texto1]').type('Gosto muito de jogos Arcade.');
+        cy.get('[id=button]').click();
+        cy.get('[id=name1]').type('Kay');
+        cy.get('[id=email1]').type('kayane@startaideia.com.br');
+        cy.get('[id=tel1]').type('999999999');
+        cy.get('[id=select1]').select('streetFighter');
+        cy.get('[id=texto1]').type('Gosto muito de jogos Arcade, principalmente de Street Fighter.');
+        cy.get('[id=button]').click();
+        cy.get('[id=name1]').type('Vagner Alves');
+        cy.get('[id=email1]').type('vagner@startaideia.com.br');
+        cy.get('[id=tel1]').type('988888888');
+        cy.get('[id=select1]').select('donkeyKong');
+        cy.get('[id=texto1]').type('Quando eu era criança, jogava muito Donkey Kong no meu emulador Zsnes');
+        cy.get('[id=button]').click();
+        cy.get('[id=name1]').type('Luiz');
+        cy.get('[id=email1]').type('luiz@startaideia.com.br');
+        cy.get('[id=tel1]').type('977777777');
+        cy.get('[id=select1]').select('pinball');
+        cy.get('[id=texto1]').type('Gosto muito de jogos Arcade, principalmente pinball');
+        cy.get('[id=button]').click();
+        cy.get('[id=name1]').type('Matheus');
+        cy.get('[id=email1]').type('matheus@startaideia.com.br');
+        cy.get('[id=tel1]').type('966666666');
+        cy.get('[id=select1]').select('marvelVsCapcom');
+        cy.get('[id=texto1]').type('Gosto muito de jogos Arcade, principalmente Marvel vs Capcom.');
+        cy.get('[id=button]').click();
+    });
+});
